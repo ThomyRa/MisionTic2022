113d19ec27bb94a8cd2dc9b61d08c53598b026af
thomyra <tjramirezr@gmail.com>
2021-06-11 17:50:00 -0500

    feat: Solucion laboratorios semana6

--- semana5/talleres/parqueader_ui/main.py
+++ semana5/talleres/parqueader_ui/main.py
@@ -12,17 +12,16 @@ import funciones_parqueadero as parq
 
 # Ejemplo de funcionamiento
 
-vehiculos_en_parqueadero={}
-memo_park=parq.crear_parqueadero(2,4)
+vehiculos_en_parqueadero = {}
+memo_park = parq.crear_parqueadero(2, 4)
 
-puesto_asignado=parq.ocupar_puesto_parqueadero("NAR-110",memo_park,vehiculos_en_parqueadero)
-print("Puesto Asignado",puesto_asignado)
-puesto_asignado=parq.ocupar_puesto_parqueadero("EPM-765",memo_park,vehiculos_en_parqueadero)
-print("Puesto Asignado",puesto_asignado)
-puesto_asignado=parq.ocupar_puesto_parqueadero("MAL-543",memo_park,vehiculos_en_parqueadero)
-print("Puesto Asignado",puesto_asignado)
-puesto_asignado=parq.ocupar_puesto_parqueadero("XLC-113",memo_park,vehiculos_en_parqueadero)
-print("Puesto Asignado",puesto_asignado)
-parq.liberar_puesto_parqueadero("MAL-543",memo_park,vehiculos_en_parqueadero)
-puesto_asignado=parq.ocupar_puesto_parqueadero("EPN-554",memo_park,vehiculos_en_parqueadero)
-print("Puesto Asignado",puesto_asignado)
+puesto_asignado = parq.ocupar_puesto_parqueadero("NAR-110", memo_park, vehiculos_en_parqueadero)
+# puesto_asignado = parq.ocupar_puesto_parqueadero("EPM-765", memo_park, vehiculos_en_parqueadero)
+# print("Puesto Asignado", puesto_asignado)
+# puesto_asignado = parq.ocupar_puesto_parqueadero("MAL-543", memo_park, vehiculos_en_parqueadero)
+# print("Puesto Asignado", puesto_asignado)
+# puesto_asignado = parq.ocupar_puesto_parqueadero("XLC-113",memo_park, vehiculos_en_parqueadero)
+# print("Puesto Asignado", puesto_asignado)
+# parq.liberar_puesto_parqueadero("MAL-543", memo_park, vehiculos_en_parqueadero)
+# puesto_asignado = parq.ocupar_puesto_parqueadero("EPN-554", memo_park, vehiculos_en_parqueadero)
+# print("Puesto Asignado", puesto_asignado)
